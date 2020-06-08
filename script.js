4608aa0761a7a5e80605b9ff36928620bc0bdc17
davidgeorgebell <bell.dave93@gmail.com>
2020-06-08 10:52:01 +0800

    convert functionality

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const fromInput = document.querySelector('from-input');
 const toSelect = document.querySelector('.from-select');
 const toEl = document.querySelector('to-amount');
 
+const ratesBase = {};
+
 function createOptions(options) {
   return Object.entries(options)
     .map(
@@ -16,6 +18,24 @@ function createOptions(options) {
     .join('');
 }
 
+async function fetchRates(base = 'GBP') {
+  const res = await fetch(`${endpoint}?base=${base}`);
+  const rates = await res.json();
+  return rates;
+}
+
+async function convert(amount, from, to) {
+  if (!ratesBase[from]) {
+    console.log(`We don't have ${from} to ${to} yet`);
+    const rates = await fetchRates(from);
+    console.log(rates);
+    ratesBase[from] = rates;
+  }
+  const rate = ratesBase[from].rates[to];
+  const convertedAmout = rate * amount;
+  console.log(convertedAmout);
+}
+convert(100, 'CAD', 'USD');
 const optionsHtml = createOptions(currencies);
 
 fromSelect.innerHTML = optionsHtml;
